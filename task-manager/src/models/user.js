97322bd3660dea11d4b634c4f059de05cd55b009
Nico Vega <nvega@fullstacklabs.co>
2021-08-13 16:38:01 -0300

    cascade delete tasks

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -90,6 +91,13 @@ userSchema.pre('save', async function (next){
     if (user.isModified('password')){
         user.password = await bcrypt.hash(user.password, 8)
     }
+    next()
+})
+
+//delete user tasks when user is removed
+userSchema.pre('delete', async function (next){
+    const user = this
+    await Task.deleteMany({ owner: user._id })
     next()
 })
 
